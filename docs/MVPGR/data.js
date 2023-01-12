c92ac197f930c0e6b0141e3a55a29ee63fd5f363
bstark2 <bstark2@ucmerced.edu>
2023-01-12 13:08:12 -0800

    MVPGR pano

---
+++ docs/MVPGR/data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0974",
+      "name": "DJI_0974",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.46348852857092915,
+        "pitch": 0.744007213625915,
+        "fov": 1.550639413454104
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.4510968383914289,
+          "pitch": 0.36583961533568754,
+          "title": "Western Lake Playa Pool",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.170566103048319,
+          "pitch": 0.21711927099532247,
+          "title": "Barn Pond",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.8330548836394662,
+          "pitch": 0.12259796864778671,
+          "title": "Lake Yosemite",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.9033955478278468,
+          "pitch": 0.05935940855707855,
+          "title": "UC Merced",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "UC Merced Vernal Pool and Grassland Reserve",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
